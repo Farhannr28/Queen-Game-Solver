8c4a441e7f2a874a0917f88b63f4d96343b09d6e
Farhannr28 <farhannafis281004@gmail.com>
2024-08-12 12:56:08 +0700

    chore: init commit

---
+++ src/main/java/com/farhannr28/queengame/App.java
@@ -0,0 +1,13 @@
+package com.farhannr28.queengame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/farhannr28/queengame/AppTest.java
@@ -0,0 +1,20 @@
+package com.farhannr28.queengame;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
